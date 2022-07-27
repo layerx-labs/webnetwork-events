4c9997d5fb9a441bc1328094677ad3a5855283ba
clarkjoao <clark.joao@gmail.com>
2022-07-27 12:13:04 +0100

    DEV-453 get oracles change event

--- modules/retrieve-bulk-actions/index.js
+++ modules/retrieve-bulk-actions/index.js
@@ -1,24 +1,10 @@
 import {
-  getBountyAmountUpdatedEvents,
-  getBountyCanceledEvents,
-  getBountyClosedEvents,
-  getBountyCreatedEvents,
-} from "./bounty.js";
-
-import { getOraclesChangedEvents } from "./oracles.js";
-
-import {
   getBountyPullRequestCanceledEvents,
   getBountyPullRequestCreatedEvents,
   getBountyPullRequestReadyForReviewEvents,
 } from "./pullrequest.js";
 
 const events = {
-  getBountyCreatedEvents,
-  getBountyCanceledEvents,
-  getBountyClosedEvents,
-  getBountyAmountUpdatedEvents,
-  getOraclesChangedEvents,
   getBountyPullRequestCreatedEvents,
   getBountyPullRequestReadyForReviewEvents,
   getBountyPullRequestCanceledEvents,
