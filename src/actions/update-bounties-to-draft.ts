81f4b44791e75629bbd1b2b766851fccedde58fb
clarkjoao <clark.joao@gmail.com>
2022-11-29 12:29:55 +0000

    DEV-970 do to all networks

--- src/actions/update-bounties-to-draft.ts
+++ src/actions/update-bounties-to-draft.ts
@@ -27,86 +27,82 @@ export async function action(query?: EventsQuery): Promise<EventsProcessed> {
   logger.info(`${name} start`);
 
   try {
-
-    if (!query?.networkName) {
-      loggerHandler.warn(`${name} missing network name query`);
-      return eventsProcessed
-    }
-
     const web3Connection = new Web3Connection({ web3Host, privateKey });
     await web3Connection.start();
 
-    const network = await db.networks.findOne({
-      where: {
-        name: { [Op.iLike]: String(query?.networkName).replaceAll(" ", "-") }
-      },
-      raw: true,
-    });
+    const where = {
+      isRegistered: true
+    } as { isRegistered: boolean, name?: {} }
 
-    if (!network) {
-      loggerHandler.warn(`${name} not found network ${query?.networkName}`);
+    if (query?.networkName)
+      where.name = { [Op.iLike]: String(query?.networkName).replaceAll(" ", "-") }
+
+    const networks = await db.networks.findAll({ where, raw: true });
+
+    if (!networks || !networks.length) {
+      loggerHandler.warn(`${name} found no networks`);
       return eventsProcessed;
     }
+    for (const { networkAddress, id: network_id, name: networkName } of networks) {
+      const _network = new Network_v2(web3Connection, networkAddress);
+      await _network.loadContract();
 
-    const { networkAddress, id: network_id, name: networkName } = network
-    const _network = new Network_v2(web3Connection, networkAddress);
-    await _network.loadContract();
+      const bounties = await db.issues.findAll({
+        where: {
+          state: "open",
+          network_id,
+        },
+        include: [
+          { association: "repository", },
+          { association: "pull_requests", },
+        ],
+      });
 
-    const bounties = await db.issues.findAll({
-      where: {
-        state: "open",
-        network_id,
-      },
-      include: [
-        { association: "repository", },
-        { association: "pull_requests", },
-      ],
-    });
+      loggerHandler.info(
+        `${name} found ${bounties?.length} opened bounties at ${networkName}`
+      );
 
-    loggerHandler.info(
-      `${name} found ${bounties?.length} opened bounties at ${networkName}`
-    );
+      const repositoriesDetails = {};
 
-    const repositoriesDetails = {};
+      const draftTime = await _network.draftTime()
+      const timeOnChain = await web3Connection.Web3.eth.getBlock(`latest`).then(({ timestamp }) => +timestamp * 1000);
 
-    const draftTime = await _network.draftTime()
-    const timeOnChain = await web3Connection.Web3.eth.getBlock(`latest`).then(({ timestamp }) => +timestamp * 1000);
+      for (const dbBounty of bounties) {
 
-    for (const dbBounty of bounties) {
+        if (dbBounty.pull_requests.length)
+          continue
 
-      if (dbBounty.pull_requests.length)
-        continue
+        const networkBounty = await _network.cidBountyId(`${dbBounty?.issueId}`).then(id => _network.getBounty(+id))
 
-      const networkBounty = await _network.cidBountyId(`${dbBounty?.issueId}`).then(id => _network.getBounty(+id))
+        if (isAfter(subMilliseconds(timeOnChain, draftTime), networkBounty.creationDate))
+          continue
 
-      if (isAfter(subMilliseconds(timeOnChain, draftTime), networkBounty.creationDate))
-        continue
+        const [owner, repo] = slashSplit(dbBounty?.repository?.githubPath);
+        const detailKey = `${owner}/${repo}`;
 
-      const [owner, repo] = slashSplit(dbBounty?.repository?.githubPath);
-      const detailKey = `${owner}/${repo}`;
+        if (!repositoriesDetails[detailKey])
+          repositoriesDetails[detailKey] =
+            await GHService.repositoryDetails(repo, owner);
 
-      if (!repositoriesDetails[detailKey])
-        repositoriesDetails[detailKey] =
-          await GHService.repositoryDetails(repo, owner);
+        const labelId = repositoriesDetails[detailKey]
+          .repository.labels.nodes.find((label) => label.name.toLowerCase() === "draft")?.id;
 
-      const labelId = repositoriesDetails[detailKey]
-        .repository.labels.nodes.find((label) => label.name.toLowerCase() === "draft")?.id;
+        if (labelId) {
+          const ghIssue = await GHService.issueDetails(repo, owner, dbBounty?.githubId as string);
+          await GHService.issueAddLabel(ghIssue.repository.issue.id, labelId);
+        }
 
-      if (labelId) {
-        const ghIssue = await GHService.issueDetails(repo, owner, dbBounty?.githubId as string);
-        await GHService.issueAddLabel(ghIssue.repository.issue.id, labelId);
+        dbBounty.state = "draft";
+        await dbBounty.save();
+
+        eventsProcessed[networkName] = {
+          ...eventsProcessed[networkName],
+          [dbBounty.issueId!.toString()]: { bounty: dbBounty, eventBlock: null }
+        };
+
+        logger.info(`${name} Parsed bounty ${dbBounty.issueId}`);
+
       }
-
-      dbBounty.state = "draft";
-      await dbBounty.save();
-
-      eventsProcessed[networkName] = {
-        ...eventsProcessed[networkName],
-        [dbBounty.issueId!.toString()]: { bounty: dbBounty, eventBlock: null }
-      };
-
-      logger.info(`${name} Parsed bounty ${dbBounty.issueId}`);
-
     }
 
   } catch (err: any) {
