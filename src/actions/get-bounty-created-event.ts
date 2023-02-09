b742680892b03808fb941aac717e3133e992b757
Vitor Hugo <vhcsilvaa@gmail.com>
2023-02-08 22:26:52 -0300

    filling new columns

--- src/actions/get-bounty-created-event.ts
+++ src/actions/get-bounty-created-event.ts
@@ -6,8 +6,10 @@ import {EventService} from "../services/event-service";
 import {BountyCreatedEvent} from "@taikai/dappkit/dist/src/interfaces/events/network-v2-events";
 import {DB_BOUNTY_NOT_FOUND, NETWORK_BOUNTY_NOT_FOUND} from "../utils/messages.const";
 import {BlockProcessor} from "../interfaces/block-processor";
-import {updateLeaderboardBounties} from "src/modules/leaderboard";
-import {updateBountiesHeader} from "src/modules/handle-header-information";
+import { updateLeaderboardBounties } from "src/modules/leaderboard";
+import { updateBountiesHeader } from "src/modules/handle-header-information";
+import {isZeroAddress} from "ethereumjs-util";
+import {isAddress} from "web3-utils";
 
 export const name = "getBountyCreatedEvents";
 export const schedule = "*/10 * * * *";
@@ -64,14 +66,19 @@ export async function action(query?: EventsQuery): Promise<EventsProcessed> {
     dbBounty.creatorGithub = bounty.githubUser;
     dbBounty.amount = bounty.tokenAmount.toString();
     dbBounty.fundingAmount = bounty.fundingAmount.toString();
+    dbBounty.rewardAmount = bounty.rewardAmount.toString();
     dbBounty.branch = bounty.branch;
     dbBounty.title = bounty.title;
     dbBounty.contractId = +id;
 
-    const tokenId = await validateToken(service.web3Connection, bounty.transactional, true, chainId);
-    if (!tokenId)
-      logger.warn(`Failed to validate token ${bounty.transactional}`)
-    else dbBounty.tokenId = tokenId;
+    await validateToken(service.web3Connection, bounty.transactional, true, chainId)
+      .then(id => dbBounty.transactionalTokenId = id)
+      .catch(error => logger.warn(`Failed to validate token ${bounty.transactional}`, error.toString()));
+
+    if (isAddress(bounty.rewardToken) && !isZeroAddress(bounty.rewardToken))
+      await validateToken(service.web3Connection, bounty.rewardToken, false, chainId)
+        .then(id => dbBounty.rewardTokenId = id)
+        .catch(error => logger.warn(`Failed to validate token ${bounty.rewardToken}`, error.toString()));
 
     await dbBounty.save();
 
