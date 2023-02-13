fc725def4f271f4843ff0ed6000983995af26df3
Marcus Vinícius <marcusvinici25@gmail.com>
2023-02-13 11:30:20 -0300

    refactoring to receive log on coingecko

--- src/services/coingecko.ts
+++ src/services/coingecko.ts
@@ -9,34 +9,40 @@ const {
 const COINGECKO_API = axios.create({baseURL: "https://api.coingecko.com/api/v3"});
 
 async function getCoinPrice(search: string, fiat = currency) {
-  try {
-    if (!enableCoinGecko)
-    return 0;
-
+    if (!enableCoinGecko){
+      logger.warn("enableCoinGecko env is disabled")
+      return 0;
+    }
+  
     const coins = await COINGECKO_API.get(`/coins/list?include_platform=false`).then(value => value.data);
+
+    if(!Array.isArray(coins))
+        logger.error(coins, "Error to get list coingecko")
+
     const symbols = search.toLowerCase().split(',')
     const coinsData = coins.filter(({symbol}) => symbols.includes(symbol))
 
-    if (coinsData.length < 1)
+    if (coinsData.length < 1){
+      logger.error(coinsData, "Error to filter symbol coingecko")
       return 0;
-
+    }
+      
     const ids = coinsData.map(({id}) => id).join()
 
     const price = await COINGECKO_API.get(`/simple/price?ids=${ids}&vs_currencies=${fiat || 'eur'}`);
 
-    if (!price?.data)
+    if (!price?.data){
+      logger.error(price, "Error to get prices coingecko")
       return 0;
+    }
+      
 
     const result: any = {}
 
     coinsData.map(({symbol, id}) => result[symbol] = {[fiat || 'eur']: price?.data?.[id]?.[fiat || 'eur']})
 
     return result
-  } catch(e) {
-    logger.error("service coingecko error:", e)
-    return 0;
-  } 
-}
+  }
 
 export {
     getCoinPrice
