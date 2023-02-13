651e2adc0d5ed50f0d5c4b223db631d194d6d997
Marcus Vinícius <marcusvinici25@gmail.com>
2023-02-13 10:56:18 -0300

    adding try catch

--- src/services/coingecko.ts
+++ src/services/coingecko.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import logger from "src/utils/logger-handler";
 
 const {
     NEXT_PUBLIC_CURRENCY_MAIN: currency,
@@ -8,16 +9,17 @@ const {
 const COINGECKO_API = axios.create({baseURL: "https://api.coingecko.com/api/v3"});
 
 async function getCoinPrice(search: string, fiat = currency) {
+  try {
     if (!enableCoinGecko)
-      return 0;
-  
+    return 0;
+
     const coins = await COINGECKO_API.get(`/coins/list?include_platform=false`).then(value => value.data);
     const symbols = search.toLowerCase().split(',')
     const coinsData = coins.filter(({symbol}) => symbols.includes(symbol))
 
     if (coinsData.length < 1)
       return 0;
-  
+
     const ids = coinsData.map(({id}) => id).join()
 
     const price = await COINGECKO_API.get(`/simple/price?ids=${ids}&vs_currencies=${fiat || 'eur'}`);
@@ -30,7 +32,11 @@ async function getCoinPrice(search: string, fiat = currency) {
     coinsData.map(({symbol, id}) => result[symbol] = {[fiat || 'eur']: price?.data?.[id]?.[fiat || 'eur']})
 
     return result
-  }
+  } catch(e) {
+    logger.error("service coingecko error:", e)
+    return 0;
+  } 
+}
 
 export {
     getCoinPrice
