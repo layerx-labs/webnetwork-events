330ea3ded00b8ccf09ada9881c87d7649a9f339d
MoshMage <moshmage@gmail.com>
2023-04-20 11:05:16 +0100

    make it so we only read one block per read-call

--- src/services/api-block-sniffer.ts
+++ src/services/api-block-sniffer.ts
@@ -4,9 +4,8 @@ import {BlockSniffer} from "src/services/block-sniffer";
 export class ApiBlockSniffer extends BlockSniffer {
   constructor(readonly web3Host: string,
               readonly mappedEventActions: MappedEventActions,
-              readonly startBlock: number = 0,
-              readonly targetBlock = 0) {
-    super(web3Host, mappedEventActions, startBlock, targetBlock, null, 0);
+              readonly startBlock: number = 0) {
+    super(web3Host, mappedEventActions, startBlock, startBlock + 1, null, 0);
   }
 
   // remove these because they are not needed on this ephemeral state
