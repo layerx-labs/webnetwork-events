0b767323cd5e3ea2d6be84cc0a5c770a417888ea
Vitor Hugo <vhcsilvaa@gmail.com>
2023-09-21 13:43:39 -0300

    Bepro 1695 modify create proposal to also support generic deliveries (#150)
    * update-models
    
    * remove issueId
    
    * remove allowMerge logic
    
    * fix import
    
    * cannot compare cid because of older prs

--- src/interfaces/bounties.ts
+++ src/interfaces/bounties.ts
@@ -1,5 +1,5 @@
 import { chainsAttributes } from "src/db/models/chains";
-import { deliverableAttributes } from "src/db/models/deliverables";
+import { deliverablesAttributes } from "src/db/models/deliverables";
 import { issuesAttributes } from "src/db/models/issues";
 import { networksAttributes } from "src/db/models/networks";
 import { repositoriesAttributes } from "src/db/models/repositories";
@@ -7,7 +7,7 @@ import { tokensAttributes } from "src/db/models/tokens";
 
 export interface Repository extends repositoriesAttributes {}
 
-export interface Deliverables extends deliverableAttributes {}
+export interface Deliverables extends deliverablesAttributes {}
 
 export interface Bounty extends issuesAttributes {
   repository?: Repository;
