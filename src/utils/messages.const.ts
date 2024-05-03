6d60ef89e512adbdf3b7a9e93af72d568d2e683d
Vitor Hugo <vhcsilvaa@gmail.com>
2024-05-03 08:54:55 -0300

    BEPRO 2172 points system action accept proposal (#199)
    * function not finished
    
    * function to save point event
    
    * use default function
    
    * saving points on bounty closed event
    
    * update councilMembers too

--- src/utils/messages.const.ts
+++ src/utils/messages.const.ts
@@ -2,6 +2,9 @@ export const NETWORK_BOUNTY_NOT_FOUND = (name, id, networkAddress) =>
   `${name} Bounty not found for id ${id} in network ${networkAddress}`;
 
 export const DB_BOUNTY_NOT_FOUND = (name, cid, networkId) =>
-  `${name} Failed to find a bounty in database matching ${cid} on network ${networkId}`
+  `${name} Failed to find a bounty in database matching ${cid} on network ${networkId}`;
 
 export const NETWORK_NOT_FOUND = (name, address) => `${name} Failed to find a network with ${address}`;
+
+export const DB_BOUNTY_ALREADY_CLOSED = (name, cid, networkId) =>
+  `${name} Bounty matching ${cid} on network ${networkId} was already closed`;
