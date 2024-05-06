58d5875ef6180bdd8608b04578e7b74be44aa185
vhcsilva <vhcsilvaa@gmail.com>
2024-05-06 13:58:51 -0300

    save tokens prices too

--- src/actions/points-system/save-locked-tokens-events.ts
+++ src/actions/points-system/save-locked-tokens-events.ts
@@ -22,16 +22,21 @@ export async function action() {
     },
     include: [
       {
-        association: "network"
+        association: "network",
+        include: [
+          { association: "network_token_token" }
+        ]
       }
     ],
   });
 
   const totalConvertedByCurator = {};
+  const tokensPrices = {};
 
   for (const curator of curators) {
     const tokenPrice = await getOrUpdateLastTokenPrice(curator.network.network_token_id!);
     const convertedAmount = +curator.tokensLocked! * tokenPrice;
+    tokensPrices[curator.network.network_token_token.symbol] = tokenPrice;
     totalConvertedByCurator[curator.address] = (totalConvertedByCurator[curator.address] || 0) + convertedAmount;
   }
 
@@ -39,6 +44,7 @@ export async function action() {
     await savePointEvent( "locked",
                           address,
                           { 
+                            tokensPrices,
                             curators: curators
                               .filter(c => c.address.toLowerCase() === address.toLowerCase())
                               .map(({ tokensLocked, networkId }) => ({ tokensLocked, networkId })) 
